0d4cc6a96546c49e061ac46c112ea335111cad9c
M-Green99 <maxgreen99@btinternet.com>
2026-08-08 23:04:31 +0100

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,24 @@
+# portfolio.py
+
+# Your portfolio holdings (tickers)
+portfolio = [
+    "RR.L", # Rolls Royce
+    "ENR.DE", # Siemens Energy
+    "GEV", # GE Vernova
+    "CEG", # Constellation Energy
+    "NEE", # NextEra Energy
+    "CCJ", # Cameco
+    "SMR", # NuScale Energy
+    "NVDA", # NVIDIA
+    "GOOGL", # Alphabet (Google)
+    "AAPL", # Apple
+    "MSFT", # Microsoft
+    "AMZN", # Amazon
+    "AVGO", # Broadcom
+    "META", # Meta
+    "IBM", # IBM
+    "QBTS", # D-Wave Quantum
+    "IONQ", # IonQ
+    "RGTI", # Rigetti Computing
+    "LMP.L", #London Metric PLC
+]
